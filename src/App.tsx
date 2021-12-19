07675c07e1ad81c9d291e2519d95bfb263495f67
garebareDA <garedaiki@gmail.com>
2021-12-19 21:41:05 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom';
 
 import {Home} from './routes/Home';
 import {Login} from './routes/Login';
 
+import { useApiKey } from './hooks/api';
+
 export const App: React.FC = () => {
+    const {getApiKey} = useApiKey();
+    const [apiKey, setApiKey] = React.useState<string | null>(null);
+
+    useEffect(() => {
+        getApiKey().then(setApiKey);
+    }, [apiKey]);
+
+    React.useEffect
     return (
         <Routes>
             <Route path="/" element={<Home />} />
