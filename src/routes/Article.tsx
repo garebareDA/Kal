6d34aca86afdf14d7a09e67c5dee7bab12d92809
garebareDA <garedaiki@gmail.com>
2022-01-01 19:13:50 +0900

    fix

--- src/routes/Article.tsx
+++ src/routes/Article.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { useAuth } from '../hooks/auth';
@@ -19,12 +19,12 @@ const Contents = styled.div`
     color: ${(props) => props.theme.contentsText};
 `
 
-const Frame = styled.p`
+const Frame = styled.div`
     max-width: 800px;
     width: 100%;
 `
 
-const Page = styled.p`
+const Page = styled.div`
     margin-left: 24px;
     margin-right: 24px;
     color:${(props) => props.theme.mainText};
@@ -34,17 +34,22 @@ export const Article: React.VFC<{apiKey:string|null, theme:string, toggleTheme:(
     const { id } = useParams();
     const { user } = useAuth();
     const {getArticle, article} = useMicrocms();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if(!user) return;
-        if(!apiKey || apiKey == "") return;
+        if(!apiKey || apiKey == "") {
+            navigate('/');
+            return;
+        }
+
         getArticle(apiKey, id);
     } , [apiKey, user]);
 
     return (
         <Articles>
             <Logo theme={theme} toggleTheme={toggleTheme}/>
-            {!article && <Page>何もありません！</Page>}
+            {!article && apiKey && <Page>読込中……</Page>}
             {article &&
                 <Frame>
                     <ArticleList title={article.title} subtitle={article.profile} id={article.id} key={article.id} createdAt={article.date} isLink={false}></ArticleList>
